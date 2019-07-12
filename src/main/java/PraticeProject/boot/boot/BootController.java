ec8f7501b6e5053d160d64b4b3d5c80e6f5a4072
緯創-徐雍俊 <F126619@OSRPC251114.outsourcing.intra.cathaybk>
2019-07-12 16:36:05 +0800

    new develop branch

--- src/main/java/PraticeProject/boot/boot/BootController.java
+++ src/main/java/PraticeProject/boot/boot/BootController.java
@@ -5,19 +5,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
 /**
  * spring boot test
+ * 
  * @author wistronits
  *
  */
 @Controller
 @EnableAutoConfiguration
 public class BootController {
+
 	@RequestMapping("/boot")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
-    }
+	@ResponseBody
+	String home() {
+
+		return "Hello World!";
+	}
 
 }
